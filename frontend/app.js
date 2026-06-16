fbb3423d94fbde3dc765694fd0c1bb6a276899f8
Jade-Eye <mohit1056may@gmail.com>
2026-06-16 12:41:38 +0530

    Docker Assignment

---
+++ frontend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/submit', async (req, res) => {
+    try {
+
+        const response = await axios.post(
+            'http://backend:5000/submit',
+            {
+                name: req.body.name,
+                email: req.body.email
+            }
+        );
+
+        res.send(response.data);
+
+    } catch (error) {
+        res.send("Error connecting backend");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Frontend running on port 3000");
+});
